8015028905c23b8a17eef5d857425423e7f6f6e1
Jannes Brunner <jannes.brunner@student.htw-berlin.de>
2018-07-25 16:19:31 +0100

    Add Like Button and toggle/display comments
    Here I used [ngClass] that toggles a class beside *ngIf and *ngFor to display an entire div block and for loop thourgh and array of photo associated comments

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -8,8 +8,15 @@ import { Component } from '@angular/core';
 
 export class EntryComponent {
     title: string = 'My first Photo';
-    photo: string = 'http://placehold.it/800x500?text=Angular+Basics';
+    photo: string = 'https://picsum.photos/800/500' //'http://placehold.it/800x500?text=Angular+Basics';
     description: string = 'A Description of my first Photo';
+    comments: any[] = [
+        { name: "John", comment: "A Comment" },
+        { name: "Frank", comment: "A Comment" },
+        { name: "Josef", comment: "A Comment" },
+        { name: "Barbara", comment: "A Comment" },
+        { name: "Peter", comment: "A Comment" },
+    ]
 }
 
 
